e584d9dd0276a0910a9570e46a8400214883388e
Łukasz Ziobrowski <andzejziobrowski@gmail.com>
2025-01-06 17:47:15 -0500

    IP: asdfgdfgfgh
    Signed-off-by: Łukasz Ziobrowski <andzejziobrowski@gmail.com>

--- frontend/src/main/java/com/example/frontend/controllers/UserController.java
+++ frontend/src/main/java/com/example/frontend/controllers/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
 
 import jakarta.servlet.http.HttpSession;
 import java.util.HashMap;
